8864f0caa5fe985f82cd541d77187071f40976e5
asmynklmn82-collab <asmynklmn82@gmail.com>
2026-05-25 23:05:43 +0300

    main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class PayPal:
         def __init__(self):
                 self.first_name = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles"]
                 self.last_name = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
-                url = 'https://sylvanhealing.org/donations-page/'
+                url = 'https://lightwithoutborders.org.es/make-a-donation'
                 parsed = urlparse(url)
                 domain = parsed.netloc
                 path = parsed.path
@@ -296,8 +296,7 @@ class PayPal:
                                 return result
                         except:
                                 return "UNKNOWN_ERROR"
-                
-                                                    
+
 # ------------------- Users -------------------
 
 ADMINS = [6843321125]  # ضع هنا ID الأدمن
